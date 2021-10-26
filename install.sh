e545e0ca2b536478ad9f5bf6b4882026cdf24bb0
Tomás Ralph <tomasralph2000@gmail.com>
2021-10-26 15:52:15 -0300

    Added neofetch config

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@
 ln -sf ~/.dotfiles/.zshrc ~
 ln -sf ~/.dotfiles/.zsh ~
 ln -sf ~/.dotfiles/.p10k.zsh ~
+
+# neofetch
+mkdir -pv ~/.config/neofetch
+ln -sf ~/.dotfiles/.config/neofetch/config.conf ~/.config/neofetch/config.conf

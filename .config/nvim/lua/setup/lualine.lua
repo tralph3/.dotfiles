e9aaaf6625b7835c86f91be4979fc3d1d9842dd7
Tomás Ralph <tomasralph2000@gmail.com>
2022-04-18 12:23:11 -0300

    enable global status line

--- .config/nvim/lua/setup/lualine.lua
+++ .config/nvim/lua/setup/lualine.lua
@@ -5,7 +5,8 @@ require'lualine'.setup{
         component_separators = { left = '', right = ''},
         section_separators = { left = '', right = ''},
         disabled_filetypes = {},
-        always_divide_middle = true
+        always_divide_middle = true,
+        globalstatus = true,
     },
 
     sections = {

586a806b54482616d8ba86f275fea16a5e167fee
Tomás Ralph <tomasralph2000@gmail.com>
2022-06-28 21:23:17 -0300

    added mappings for default commands

--- .config/nvim/lua/setup/neoprojet.lua
+++ .config/nvim/lua/setup/neoprojet.lua
@@ -1,4 +1,7 @@
-require('neoprojet').setup({
+local map = require('config.utils').map
+local np = require('neoprojet')
+
+np.setup({
     sessions = false,
 })
 
@@ -7,3 +10,7 @@ local status_ok, telescope = pcall(require, 'telescope')
 if status_ok then
     telescope.load_extension('neoprojet')
 end
+
+map('n', '<F5>', function() np.call_command('build_and_run') end)
+map('n', 'S-<F5>', function() np.call_command('run') end)
+map('n', '<F4>', function() np.call_command('run_tests') end)

fa60ad3b47ff1703e1708eff47a016952574c105
Tomás Ralph <tomasralph2000@gmail.com>
2022-07-10 23:51:26 -0300

    added more commands

--- .config/awesome/commands.lua
+++ .config/awesome/commands.lua
@@ -1,6 +1,19 @@
+local settings = require('settings')
+
 local commands = {
-    terminal = 'alacritty',
+    terminal='alacritty',
+    raise_volume='/usr/bin/pactl set-sink-volume 0 +'..settings.volume_step,
+    lower_volume='/usr/bin/pactl set-sink-volume 0 -'..settings.volume_step,
+    toggle_mute_audio='pactl set-sink-mute 0 toggle',
+    toggle_mute_mic='amixer set Capture toggle',
     run_prompt = 'rofi -show drun',
+    browser='firefox',
+    browser_private='firefox --private-window',
+    screenshot='flameshot gui',
+    poweroff='poweroff',
+    reboot='reboot',
+    brightness_up='light -A '..settings.brightness_step,
+    brightness_down='light -U '..settings.brightness_step,
 }
 
 return commands

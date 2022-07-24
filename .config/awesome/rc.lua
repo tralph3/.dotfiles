708e539a137340c95c72f299cbc55e393a494265
Tomás Ralph <tomasralph2000@gmail.com>
2022-07-24 12:54:07 -0300

    Fix window tag restrictions

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -2,11 +2,11 @@ package.loaded['naughty.dbus'] = {}
 
 require('autostart')
 require('awful.autofocus')
-require('rules')
 require('errors')
 require('layouts')
 require('bar')
 require('signals')
+require('rules')
 local beautiful = require('beautiful')
 
 local theme_path = string.format(

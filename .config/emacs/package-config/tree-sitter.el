cc7a7d339ea94e7c633c4afa47c684f99ece370a
Tomás Ralph <tomasralph2000@gmail.com>
2022-08-08 20:49:43 -0300

    fix c-u scrolling, fix treemacs theme

--- .config/emacs/package-config/tree-sitter.el
+++ .config/emacs/package-config/tree-sitter.el
@@ -1,3 +1,3 @@
 (global-tree-sitter-mode)
 (push (expand-file-name "~/.local/share/treesitter-parsers/parser") tree-sitter-load-path)
-(tree-sitter-require 'python)
+(add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)

7b71cf83a28e6b1748a1fd3f7b34bc08add49f97
Tomás Ralph <tomasralph2000@gmail.com>
2022-08-09 05:42:13 -0300

    added org-bullets

--- .config/emacs/package-config/evil.el
+++ .config/emacs/package-config/evil.el
@@ -1,4 +1,3 @@
-(setq evil-want-C-u-scroll t)
 (setq evil-undo-system 'undo-redo)
 (define-key evil-normal-state-map "H" 'evil-first-non-blank)
 (define-key evil-normal-state-map "L" 'evil-end-of-line)

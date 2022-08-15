7e8262538e048ad5dd2badc89d5c825a21c2a4ce
Tomás Ralph <tomasralph2000@gmail.com>
2022-08-15 04:20:01 -0300

    always ensure packages, redirect lock files

--- .config/emacs/packages.el
+++ .config/emacs/packages.el
@@ -11,70 +11,58 @@
 
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
+(setq use-package-always-ensure t)
 (require 'use-package)
 
 (use-package evil
   :init
   (setq evil-want-C-u-scroll t)
-  :ensure t
   :config (get-config "evil"))
 
 (use-package centaur-tabs
-  :ensure t
   :config (get-config "centaur-tabs"))
 
 (use-package doom-modeline
-  :ensure t
   :config (get-config "doom-modeline"))
 
 (use-package vterm
-  :ensure t
   :config (get-config "vterm"))
 
 (use-package all-the-icons
-  :ensure t
   :config (get-config "all-the-icons"))
 
 (use-package treemacs
-  :ensure t
   :config (get-config "treemacs"))
 
 (use-package treemacs-evil
-  :ensure t
   :config (get-config "treemacs-evil"))
 
 (use-package treemacs-all-the-icons
-  :ensure t
   :config (get-config "treemacs-all-the-icons"))
 
 (use-package helm
-  :ensure t
   :config (get-config "helm"))
 
 (use-package tree-sitter
-  :ensure t
   :config (get-config "tree-sitter"))
 
 (use-package tree-sitter-langs
-  :ensure t
   :config (get-config "tree-sitter-langs"))
 
 (use-package lsp-mode
-  :ensure t
   :config (get-config "lsp-mode"))
 
 (use-package company
-  :ensure t
   :config (get-config "company"))
 
 (use-package rust-mode
-  :ensure t
   :config (get-config "rust-mode"))
 
 (use-package doom-themes
-  :ensure t
   :config (get-config "doom-themes"))
 
 (use-package org-bullets
-  :ensure t
   :config (org-bullets-mode 1))
+
+(use-package dap-mode
+  :config (get-config "dap-mode"))

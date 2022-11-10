801244d0f210d4070a8eb5bb643b9509459c5b42
Tomás Ralph <tomasralph2000@gmail.com>
2022-11-09 23:54:58 -0300

    support multiple icons and multiple themes

--- .config/qtile/brightness.sh
+++ .config/qtile/brightness.sh
@@ -1,5 +1,25 @@
+get_icon_theme_name () {
+    grep "gtk-icon-theme-name" ~/.config/gtk-3.0/settings.ini | cut -d "=" -f 2
+}
+
 get_brightness () {
     light
+}
+
+get_brightness_icon () {
+    ICON_THEME=$(get_icon_theme_name)
+    if ((0==$1))
+    then
+        echo "/usr/share/icons/$ICON_THEME/symbolic/status/display-brightness-off-symbolic.svg"
+    elif ((1<=$1 && $1<=33))
+    then
+        echo "/usr/share/icons/$ICON_THEME/symbolic/status/display-brightness-low-symbolic.svg"
+    elif ((34<=$1 && $1<=66))
+    then
+        echo "/usr/share/icons/$ICON_THEME/symbolic/status/display-brightness-medium-symbolic.svg"
+    else
+        echo "/usr/share/icons/$ICON_THEME/symbolic/status/display-brightness-high-symbolic.svg"
+    fi
 }
 
 raise_brightness () {
@@ -14,7 +34,8 @@ lower_brightness () {
 
 show_notification () {
     BRIGHTNESS=$(get_brightness)
-    dunstify -h int:value:$BRIGHTNESS -t 1000 -I /usr/share/icons/Papirus-Dark/16x16/actions/brightnesssettings.svg -u normal -r 2593 "Brightness"
+    ICON=$(get_brightness_icon $BRIGHTNESS)
+    dunstify -h int:value:$BRIGHTNESS -t 1000 -I $ICON -u normal -r 2593 "Screen Brightness"
 }
 
 case $1 in
--- .config/qtile/volume.sh
+++ .config/qtile/volume.sh
@@ -1,6 +1,32 @@
+get_icon_theme_name () {
+    grep "gtk-icon-theme-name" ~/.config/gtk-3.0/settings.ini | cut -d "=" -f 2
+}
+
 get_volume () {
-    volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | cut -d " " -f 2)
+    mute_check=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -o MUTED)
+    if [[ "MUTED" == $mute_check ]]
+    then
+        volume=0.00
+    else
+        volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | cut -d " " -f 2)
+    fi
     echo $volume*10 | awk -v volume=$volume "{print(volume*100)}"
+}
+
+get_volume_icon () {
+    ICON_THEME=$(get_icon_theme_name)
+    if ((0==$1))
+    then
+        echo "/usr/share/icons/$ICON_THEME/22x22/panel/audio-volume-muted.svg"
+    elif ((1<=$1 && $1<=33))
+    then
+        echo "/usr/share/icons/$ICON_THEME/22x22/panel/audio-volume-low.svg"
+    elif ((34<=$1 && $1<=66))
+    then
+        echo "/usr/share/icons/$ICON_THEME/22x22/panel/audio-volume-medium.svg"
+    else
+        echo "/usr/share/icons/$ICON_THEME/22x22/panel/audio-volume-high.svg"
+    fi
 }
 
 get_sink_name () {
@@ -17,13 +43,20 @@ lower_volume () {
     show_notification
 }
 
+toggle_mute_volume () {
+    pactl set-sink-mute 0 toggle
+    show_notification
+}
+
 show_notification () {
     VOLUME=$(get_volume)
     SINK_NAME=$(get_sink_name)
-    dunstify -h int:value:$VOLUME -t 1000 -I /usr/share/icons/Papirus-Dark/16x16@2x/panel/audio-volume-high.svg -u normal -r 2593 "$SINK_NAME"
+    ICON=$(get_volume_icon $VOLUME)
+    dunstify -h int:value:$VOLUME -t 1000 -I $ICON -u normal -r 2593 "$SINK_NAME"
 }
 
 case $1 in
     raise) raise_volume $2 ;;
     lower) lower_volume $2 ;;
+    mute)  toggle_mute_volume ;;
 esac

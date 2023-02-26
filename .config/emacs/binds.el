48c42fd39cb332558393cb3abf4ed2e42c9bb861
Tomás Ralph <tomasralph2000@gmail.com>
2023-02-26 06:18:25 -0300

    initial new emacs config

---
+++ .config/emacs/binds.el
@@ -0,0 +1,13 @@
+(defun insert-blank-line-top ()
+  (interactive)
+  (move-beginning-of-line nil)
+  (open-line 1))
+
+(defun insert-blank-line-bottom ()
+  (interactive)
+  (move-end-of-line nil)
+  (open-line 1)
+  (next-line 1))
+
+(global-set-key (kbd "M-o") 'insert-blank-line-bottom)
+(global-set-key (kbd "M-O") 'insert-blank-line-top)
---
+++ .config/emacs/colorscheme.el
@@ -0,0 +1,4 @@
+(load-file "~/.config/colorschemes/current_colorscheme/colors.el")
+(add-to-list 'default-frame-alist `(background-color . ,BACKGROUND_1))
+(add-to-list 'default-frame-alist `(foreground-color . ,FOREGROUND_1))
+(set-face-background 'fringe BACKGROUND_1)

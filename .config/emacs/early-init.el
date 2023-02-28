5bf91a0826e218f28e9a8755b465327ba32e2828
Tomás Ralph <tomasralph2000@gmail.com>
2023-02-27 22:22:30 -0300

    fix package initialization

---
+++ .config/emacs/early-init.el
@@ -0,0 +1,10 @@
+(setq user-emacs-directory "~/.local/share/emacs/")
+(setq user-config-directory "~/.config/emacs/")
+
+(setq-default create-lockfiles nil)
+(setq-default make-backup-files nil)
+(setq-default backup-inhibited t)
+(setq-default auto-save-list-file-prefix (concat user-emacs-directory "auto-saves/.saves-"))
+(setq-default custom-file (concat user-emacs-directory "custom-file"))
+(setq-default package-user-dir (concat user-emacs-directory "elpa"))
+(setq-default url-history-file (concat user-emacs-directory "url/history"))

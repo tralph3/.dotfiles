0e121d6a4674e0cf6b29f59a8401ff02c09b9f86
Tomás Ralph <tomasralph2000@gmail.com>
2023-02-28 05:00:52 -0300

    added treemacs, treesitter, rust, and delete whitespace

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -3,9 +3,17 @@
 (load-file (concat user-config-directory "packages.el"))
 
 (setq display-line-numbers-type 'relative)
-(tab-line-mode 1)
-(global-display-line-numbers-mode 1)
+(global-display-line-numbers-mode t)
 (scroll-bar-mode 0)
+(tool-bar-mode 0)
+(menu-bar-mode 0)
 (fset 'yes-or-no-p 'y-or-n-p)
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq-default whitespace-style '(trailing))
+(global-whitespace-mode t)
+(setq inhibit-startup-screen t)
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 (load custom-file :noerror)

92d250c3f3aadb76ee54a4485a2fab1223b219dc
Tomás Ralph <tomasralph2000@gmail.com>
2023-03-20 20:51:40 -0300

    Improve custom colorscheme (WIP)

--- .config/emacs/colorscheme.el
+++ .config/emacs/colorscheme.el
@@ -1,4 +1,18 @@
 (load-file "~/.config/colorschemes/current_colorscheme/colors.el")
-;(add-to-list 'default-frame-alist `(background-color . ,BACKGROUND_1))
-;(add-to-list 'default-frame-alist `(foreground-color . ,FOREGROUND_1))
-;(set-face-background 'fringe BACKGROUND_1)
+
+; BASIC COLORS
+(setq-default default-frame-alist `((background-color . ,BACKGROUND_1) (foreground-color . ,FOREGROUND_1)))
+(set-face-background 'fringe BACKGROUND_1)
+
+(set-face-background 'cursor ACCENT_1)
+
+; MODELINE
+(set-face-background 'mode-line BACKGROUND_2)
+(set-face-foreground 'mode-line FOREGROUND_1)
+(set-face-background 'mode-line-inactive BACKGROUND_1)
+(set-face-background 'mode-line-highlight ACCENT_1)
+(set-face-foreground 'mode-line-highlight FOREGROUND_2)
+
+; CORFU
+(set-face-background 'corfu-default BACKGROUND_2)
+(set-face-foreground 'corfu-default FOREGROUND_1)

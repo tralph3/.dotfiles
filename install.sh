adf491474d6704b3802731eb5bbbc4f55aa40b4d
tralph3 <tomasralph2000@gmail.com>
2023-10-19 18:00:12 -0300

    Update install script

--- install.sh
+++ install.sh
@@ -7,129 +7,21 @@ VARIABLES=$(cat << EOF
 
 export DOTFILES_DIR=$DOTFILES_DIR
 export QT_QPA_PLATFORMTHEME=qt5ct
-export WALLPAPERS_DIR="/usr/share/my_wallpapers"
-export GTK_THEME=Breeze-Dark
+export WALLPAPERS_DIR="/usr/share/wallpapers"
+export GTK_THEME=adw-gtk3
+export ZDOTDIR=/home/tralph3/.config/zsh
+export ANDROID_HOME=/home/tralph3/.local/share/android
+export ANDROID_SDK_ROOT=$ANDROID_HOME
 EOF
 )
-DEFAULT_COLORSCHEME=$DOTFILES_DIR/.config/colorschemes/Catppuccin.scheme
 
-NO_CONFIRM=0
+DEFAULT_COLORSCHEME="$DOTFILES_DIR/assets/colorschemes/gruvbox-material-dark-soft.el"
 
-if [ -n "$1" ]; then
-    case $1 in
-        --noconfirm)
-            NO_CONFIRM=1
-            ;;
-    esac
+if ! [ -f "~/.config/current_colorscheme" ]; then
+    source "$DOTFILES_DIR/scripts/change_colorscheme.sh" "-c$DEFAULT_COLORSCHEME"
 fi
 
-install_config() {
-    unset REPLY
-    # first argument is relative to dotfiles dir
-    ORIGIN=$DOTFILES_DIR/$1
-    DEST=$2
-
-    # if there's nothing, install
-    if ! [ -a "$DEST" ]; then
-        mkdir -pv "$(dirname "$DEST")"
-        ln -sfv "$ORIGIN" -T "$DEST"
-        # empty line
-        echo
-    # if they're not the same file, ask
-    elif ! [ "$ORIGIN" -ef "$DEST" ]; then
-        if [ $NO_CONFIRM -eq 0 ]; then
-            echo "$DEST already exists"
-            read -p "Overwrite? [y/N]: " -n 1 -r
-            # emtpy line
-            echo
-            overwrite
-        else
-            overwrite
-        fi
-    fi
-}
-
-overwrite() {
-    if [[ "$REPLY" =~ ^[Yy]$ ]] || [ $NO_CONFIRM -eq 1 ]; then
-        mkdir -pv "$(dirname "$DEST")"
-        rm -rfv "$DEST"
-        ln -sfv "$ORIGIN" -T "$DEST"
-        # empty line
-        echo
-    fi
-}
-
-# zsh
-install_config .zshrc ~/.zshrc
-install_config .zsh ~/.zsh
-install_config .p10k.zsh ~/.p10k.zsh
-
-# neofetch
-install_config .config/neofetch/config.conf ~/.config/neofetch/config.conf
-
-# neovim
-install_config .config/nvim ~/.config/nvim
-
-# alacritty
-install_config .config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
-
-# htop
-install_config .config/htop/htoprc ~/.config/htop/htoprc
-
-# firefox
-MOZ_DIR=$(echo ~/.mozilla/firefox/*.default-release)
-install_config .mozilla/firefox/chrome "$MOZ_DIR"/chrome
-install_config .mozilla/firefox/user.js "$MOZ_DIR"/user.js
-
-# qtile
-install_config .config/qtile ~/.config/qtile
-
-# awesome
-install_config .config/awesome ~/.config/awesome
-
-# picom
-install_config .config/picom/picom.conf ~/.config/picom/picom.conf
-
-# dunst
-install_config .config/dunst/dunstrc ~/.config/dunst/dunstrc
-
-# thunar
-install_config .config/xfce4 ~/.config/xfce4
-install_config .config/Thunar ~/.config/Thunar
-
-# gtk
-install_config .config/gtk-3.0 ~/.config/gtk-3.0
-
-# qt
-install_config .config/qt5ct ~/.config/qt5ct
-
-# rofi
-install_config .config/rofi ~/.config/rofi
-
-# zathura
-install_config .config/zathura ~/.config/zathura
-
-# emacs
-install_config .config/emacs ~/.config/emacs
-
-# eww
-install_config .config/eww ~/.config/eww
-
-# hyprland
-install_config .config/hypr ~/.config/hypr
-
-# waybar
-install_config .config/waybar ~/.config/waybar
-
-# colorschemes
-install_config .config/colorschemes ~/.config/colorschemes
-
-if ! [ -f "~/.config/colorschemes/current_colorscheme/colors.sh" ]; then
-    $DOTFILES_DIR/scripts/colorscheme/generate_colorscheme_files.sh $DEFAULT_COLORSCHEME
-fi
-
-# dunst colors
-install_config .config/colorschemes/current_colorscheme/colors.ini ~/.config/dunst/dunstrc.d/10-colors.conf
+source "$DOTFILES_DIR/scripts/tangle_all_configs.sh"
 
 if [[ $USER_ID -ne 0 ]]; then
     echo "Root access is needed to create a file that will set environment variables on startup."
--- scripts/change_colorscheme.sh
+++ scripts/change_colorscheme.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
 
+DONT_RELOAD=
+COLORSCHEME_PATH=
+COLORSCHEME_NAME=
 COLORSCHEME_DIRECTORY="$DOTFILES_DIR/assets/colorschemes"
 
+while getopts "rc:" opt; do
+    case $opt in
+    r) DONT_RELOAD=1 ;;
+    c) COLORSCHEME_PATH=$OPTARG;;
+    ?) echo "Unknown option $opt"; exit 2;;
+    esac
+done
+
 show_colorscheme_chooser() {
-    COLORSCHEME=$(\
+    COLORSCHEME_NAME=$(\
         basename -s .el $(ls -1 $COLORSCHEME_DIRECTORY) |\
         rofi -dmenu -p "Choose colorscheme"
     )
-    if [[ -z $COLORSCHEME ]]; then
+    if [[ -z "$COLORSCHEME_NAME" ]]; then
         exit
     fi
 }
 
-
-if [[ -z "$1" ]]; then
+if [[ -z "$COLORSCHEME_PATH" ]]; then
     show_colorscheme_chooser
-else
-    COLORSCHEME="$1"
+    COLORSCHEME_PATH="$COLORSCHEME_DIRECTORY/$COLORSCHEME_NAME.el"
 fi
 
 apply_colorscheme() {
-    ln -sf "$COLORSCHEME_DIRECTORY/$COLORSCHEME.el" ~/.config/current_colorscheme
+    ln -sf "$COLORSCHEME_PATH" ~/.config/current_colorscheme
 }
 
 generate_config_files() {
@@ -31,7 +40,16 @@ reload_all_programs() {
     "$DOTFILES_DIR/scripts/reload_programs.sh"
 }
 
-echo "Applying colorscheme \"$COLORSCHEME\""
+if ! [ -f "$COLORSCHEME_PATH" ]; then
+    echo "No such file or directory $COLORSCHEME_PATH"
+    exit 1
+fi
+
+echo "Applying colorscheme \"$COLORSCHEME_PATH\""
 apply_colorscheme
+echo "Generating config files..."
 generate_config_files
-reload_all_programs
+if [ -z $DONT_RELOAD ]; then
+    echo "Reloading programs..."
+    reload_all_programs
+fi

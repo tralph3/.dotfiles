b2e0ffea58926f54e357846f01a4e24576a41547
tralph3 <tomasralph2000@gmail.com>
2024-05-19 23:48:19 -0300

    Allow to authenticate as root with keyring when tangling

--- scripts/tangle_all_configs.sh
+++ scripts/tangle_all_configs.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
-emacs --batch -l ob -l ob-shell --eval '
-(let ((org-confirm-babel-evaluate nil))
-  (with-current-buffer (find-file-noselect (file-name-concat (getenv "DOTFILES_DIR") "README.org"))
-    (org-babel-execute-buffer)))
-'
+emacs --batch -l ob -l ob-shell --eval "
+(progn
+  (setq auth-sources '(\"secrets:Login\"))
+  (let ((org-confirm-babel-evaluate nil))
+    (with-current-buffer
+        (find-file-noselect (file-name-concat (getenv \"DOTFILES_DIR\") \"README.org\"))
+      (org-babel-execute-buffer))))"

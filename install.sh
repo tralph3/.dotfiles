54162cf69a513b941630e31b66a91936e712fafd
tralph3 <tomasralph2000@gmail.com>
2024-05-20 03:48:02 -0300

    Manage dotfiles installation from README.org

--- install.sh
+++ install.sh
@@ -1,31 +1,3 @@
 #!/bin/bash
-
 DOTFILES_DIR=$(dirname "$(realpath "$0")")
-USER_ID=$(id -u)
-VARIABLES=$(cat << EOF
-#!/bin/bash
-
-export DOTFILES_DIR=$DOTFILES_DIR
-export QT_QPA_PLATFORMTHEME=qt5ct
-export WALLPAPERS_DIR="/home/tralph3/.local/share/wallpapers"
-export GTK_THEME=adw-gtk3
-export ZDOTDIR=/home/tralph3/.config/zsh
-export ANDROID_HOME=/home/tralph3/.local/share/android
-export ANDROID_SDK_ROOT=$ANDROID_HOME
-EOF
-)
-
-DEFAULT_COLORSCHEME="$DOTFILES_DIR/assets/colorschemes/gruvbox-material-dark-soft.el"
-
-if ! [ -f "~/.config/current_colorscheme" ]; then
-    source "$DOTFILES_DIR/scripts/change_colorscheme.sh" "-c$DEFAULT_COLORSCHEME"
-fi
-
-source "$DOTFILES_DIR/scripts/tangle_all_configs.sh"
-
-if [[ $USER_ID -ne 0 ]]; then
-    echo "Root access is needed to create a file that will set environment variables on startup."
-    echo "$VARIABLES" | sudo tee /etc/profile.d/dotfiles.sh
-else
-    echo "$VARIABLES" > /etc/profile.d/dotfiles.sh
-fi
+"$DOTFILES_DIR/scripts/tangle_all_configs.sh"
--- scripts/tangle_all_configs.sh
+++ scripts/tangle_all_configs.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
+DOTFILES_DIR=$(realpath "$(dirname "$(realpath "$0")")/..")
+
 emacs --batch -l ob -l ob-shell --eval "
 (progn
   (setq auth-sources '(\"secrets:Login\"))
+  (setq dotfiles-path \"$DOTFILES_DIR\")
   (let ((org-confirm-babel-evaluate nil))
     (with-current-buffer
-        (find-file-noselect (file-name-concat (getenv \"DOTFILES_DIR\") \"README.org\"))
+        (find-file-noselect (file-name-concat dotfiles-path \"README.org\"))
       (org-babel-execute-buffer))))"

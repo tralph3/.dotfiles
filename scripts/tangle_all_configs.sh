0742ba0d9ef9b2b2cce5bf5f9f74c45c6e207214
tralph3 <tomasralph2000@gmail.com>
2024-10-27 14:59:04 -0300

    improve tangling shell output

--- scripts/tangle_all_configs.sh
+++ scripts/tangle_all_configs.sh
@@ -1,11 +1,12 @@
-#! /bin/bash
+#!/bin/bash
 DOTFILES_DIR=$(realpath "$(dirname "$(realpath "$0")")/..")
 
 emacs --batch -l ob -l ob-shell --eval "
 (progn
   (setq auth-sources '(\"secrets:Login\"))
   (setq dotfiles-path \"$DOTFILES_DIR\")
-  (let ((org-confirm-babel-evaluate nil))
+  (let ((org-confirm-babel-evaluate nil)
+        (inhibit-message t))
     (with-current-buffer
         (find-file-noselect (file-name-concat dotfiles-path \"README.org\"))
       (org-babel-execute-buffer))))"
